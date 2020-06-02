620209fac551a2b7c77f3fd01fb96540e06e13a6
Joel Saupe <joel@joelsaupe.com>
2020-06-02 11:15:29 -0600

    .

--- src/providers/ApiProvider.jsx
+++ src/providers/ApiProvider.jsx
@@ -1,34 +1,20 @@
 import React from 'react';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
+import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 // Instantiate required constructor fields
 const cache = new InMemoryCache();
 
-const httpLink = createHttpLink({
+const link = new HttpLink({
   uri: "https://staging.selfdetermine.net/graphql",
-  credentials: "omit",
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = window.sessionStorage.getItem('cdatribe:token');
-
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
+  credentials: "include",
 });
 
 const client = new ApolloClient({
+  link,
   cache,
-  link: authLink.concat(httpLink),
 });
 
 export const ApiProvider = props => <ApolloProvider {...props} client={client} />;
